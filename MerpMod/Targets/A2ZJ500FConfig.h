da5bc9a95a6b62912d9e2e8634478cdd38e1b0ec
Merp <merrillamyersiii@gmail.com>
2014-05-12 20:12:11 -0400

    Added header, config, and linker script for A2ZJ500F.

---
+++ MerpMod/Targets/A2ZJ500FConfig.h
@@ -0,0 +1,17 @@
+
+//////////////////////////////
+//		OPTIONS
+//////////////////////////////
+
+#define AUTO_TRANS 0
+#define SD_DMAP		0
+					
+#define ECU_IDENTIFIER_CHARS (10)
+#define ECU_CALIBRATION_CHARS (8)
+#define MOD_CALIBRATION_ID	FFFFFFFF
+#define MOD_ECU_IDENTIFIER FFFFFFFFFF
+
+#define EXPECTED_MAF_SENSOR 3.156494f
+#define EXPECTED_MAF_SD 3.73053f
+
+#define ARCH_SH7055 1
