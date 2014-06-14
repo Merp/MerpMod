2b512690d40f41f4afa0e77ff98f691651a5949c
Merp <merrillamyersiii@gmail.com>
2014-06-14 10:54:59 -0400

    Updated header and linker script for A8DK101X. Added flash testing patch for A8DK101X.

--- MerpMod/Targets/A8DK101X.h
+++ MerpMod/Targets/A8DK101X.h
@@ -1,7 +1,7 @@
-#define MOD_DATE 13.5.7.1023
+#define MOD_DATE 14.6.13.220
 
-#include "Gratis.h"
-#define MOD_CONFIG Gratis
+#include "Flash.h"
+#define MOD_CONFIG Flash
 #define MOD_BUILD Testing
 #define MOD_RELEASE 0
 #define ECU_CALIBRATION_ID A8DK101X
@@ -14,8 +14,14 @@
 #define dEcuId (0x000D0020)
 #define dRomHoleStart (0x000E1000)
 #define pRamHoleStart (0xFFFF9B60)
+#define pRamHoleEnd (0xFFFFBF70)
 #define sPull2DFloat (0x0000209C)
 #define sPull3DFloat (0x00002150)
+
+/////////////////////
+// Switch Hacks
+/////////////////////
+
 
 /////////////////////
 // Rev Limit Hack
@@ -24,7 +30,7 @@
 #define hRevLimDelete (0x0001533C)
 #define sRevLimStart (0x00034A80)
 #define sRevLimEnd (0x00034AB6)
-#define pFlagsRevLim ((char*)0xFFFF75A8)
+#define pFlagsRevLim ((unsigned char*)0xFFFF75A8)
 #define RevLimBitMask (0x80)
 
 /////////////////////
@@ -46,7 +52,7 @@
 
 #define sCelTrigger (0x0008E9CC)
 #define hCelSignal (0x0008EAE4)
-#define pCelSignalOem ((char*)0xFFFF9712)
+#define pCelSignalOem ((unsigned char*)0xFFFF9712)
 
 /////////////////////
 // Boost Hacks
@@ -88,22 +94,22 @@
 // Flags-Signals
 /////////////////////
 
-#define pResumeFlags ((char*)0xFFFF650E)
-#define ResumeBitMask ((char)0x01)
-#define pCoastFlags ((char*)0xFFFF650D)
-#define CoastBitMask ((char)0x01)
-#define pBrakeFlags ((char*)0xFFFF650F)
-#define BrakeBitMask ((char)0x01)
-#define pClutchFlags ((char*)0xFFFF6A89)
-#define ClutchBitMask ((char)0x80)
+#define pResumeFlags ((unsigned char*)0xFFFF650E)
+#define ResumeBitMask ((unsigned char)0x01)
+#define pCoastFlags ((unsigned char*)0xFFFF650D)
+#define CoastBitMask ((unsigned char)0x01)
+#define pBrakeFlags ((unsigned char*)0xFFFF650F)
+#define BrakeBitMask ((unsigned char)0x01)
+#define pClutchFlags ((unsigned char*)0xFFFF6A89)
+#define ClutchBitMask ((unsigned char)0x80)
 
 /////////////////////
 // NonSpecific Engine params
 /////////////////////
 
-#define pFbkc1 ((char*)0xFFFF6C04)
+#define pFbkc1 ((unsigned char*)0xFFFF6C04)
 #define pFbkc4 ((float*)0xFFFF78C0)
-#define pIam1 ((char*)0xFFFF6C09)
+#define pIam1 ((unsigned char*)0xFFFF6C09)
 #define pIam4 ((float*)0xFFFF2E38)
 #define pEngineSpeed ((float*)0xFFFF6AB0)
 #define pVehicleSpeed ((float*)0xFFFF6AA8)
@@ -116,7 +122,7 @@
 #define pEngineLoad ((float*)0xFFFF68CC)
 #define pReqTorque ((float*)0xFFFF7BB4)
 #define pThrottlePlate ((float*)0xFFFF67B4)
-#define pCurrentGear ((char*)0xFFFF6BB9)
+#define pCurrentGear ((unsigned char*)0xFFFF6BB9)
 #define pAf1Res ((float*)0xFFFF5F10)
 
 /////////////////////
