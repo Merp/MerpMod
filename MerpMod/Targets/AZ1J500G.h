f56dda63c2beb88ee85f31382c6aedcc197caa71
Merp <merrillamyersiii@gmail.com>
2014-07-11 21:26:40 -0400

    Update linker script and header for AZ1J500G Flash Testing.

--- MerpMod/Targets/AZ1J500G.h
+++ MerpMod/Targets/AZ1J500G.h
@@ -1,7 +1,7 @@
-#define MOD_DATE 13.5.6.224
+#define MOD_DATE 14.7.11.2124
 
-#include "Gratis.h"
-#define MOD_CONFIG Gratis
+#include "Flash.h"
+#define MOD_CONFIG Flash
 #define MOD_BUILD Testing
 #define MOD_RELEASE 0
 #define ECU_CALIBRATION_ID AZ1J500G
@@ -14,8 +14,14 @@
 #define dEcuId (0x000D5310)
 #define dRomHoleStart (0x000E1000)
 #define pRamHoleStart (0xFFFFA7E0)
+#define pRamHoleEnd (0xFFFFBF70)
 #define sPull2DFloat (0x000BE774)
 #define sPull3DFloat (0x000BE828)
+
+/////////////////////
+// Switch Hacks
+/////////////////////
+
 
 /////////////////////
 // Rev Limit Hack
@@ -24,7 +30,7 @@
 #define hRevLimDelete (0x000440F8)
 #define sRevLimStart (0x000363E0)
 #define sRevLimEnd (0x00036420)
-#define pFlagsRevLim ((char*)0xFFFF76FC)
+#define pFlagsRevLim ((unsigned char*)0xFFFF76FC)
 #define RevLimBitMask (0x01)
 
 /////////////////////
@@ -46,7 +52,7 @@
 
 #define sCelTrigger (0x0007FA40)
 #define hCelSignal (0x0007FAC4)
-#define pCelSignalOem ((char*)0xFFFF9C12)
+#define pCelSignalOem ((unsigned char*)0xFFFF9C12)
 
 /////////////////////
 // Boost Hacks
@@ -83,22 +89,22 @@
 // Flags-Signals
 /////////////////////
 
-#define pResumeFlags ((char*)0xFFFF622B)
-#define ResumeBitMask ((char)0x01)
-#define pCoastFlags ((char*)0xFFFF622A)
-#define CoastBitMask ((char)0x01)
-#define pBrakeFlags ((char*)0xFFFF622C)
-#define BrakeBitMask ((char)0x01)
-#define pClutchFlags ((char*)0xFFFF683B)
-#define ClutchBitMask ((char)0x01)
+#define pResumeFlags ((unsigned char*)0xFFFF622B)
+#define ResumeBitMask ((unsigned char)0x01)
+#define pCoastFlags ((unsigned char*)0xFFFF622A)
+#define CoastBitMask ((unsigned char)0x01)
+#define pBrakeFlags ((unsigned char*)0xFFFF622C)
+#define BrakeBitMask ((unsigned char)0x01)
+#define pClutchFlags ((unsigned char*)0xFFFF683B)
+#define ClutchBitMask ((unsigned char)0x01)
 
 /////////////////////
 // NonSpecific Engine params
 /////////////////////
 
-#define pFbkc1 ((char*)0xFFFF6ADD)
+#define pFbkc1 ((unsigned char*)0xFFFF6ADD)
 #define pFbkc4 ((float*)0xFFFF7BD4)
-#define pIam1 ((char*)0xFFFF6ADF)
+#define pIam1 ((unsigned char*)0xFFFF6ADF)
 #define pIam4 ((float*)0xFFFF2DE4)
 #define pEngineSpeed ((float*)0xFFFF688C)
 #define pVehicleSpeed ((float*)0xFFFF6868)
@@ -111,7 +117,7 @@
 #define pEngineLoad ((float*)0xFFFF664C)
 #define pReqTorque ((float*)0xFFFF7F38)
 #define pThrottlePlate ((float*)0xFFFF6540)
-#define pCurrentGear ((char*)0xFFFF6A79)
+#define pCurrentGear ((unsigned char*)0xFFFF6A79)
 #define pAf1Res ((float*)0xFFFF40C8)
 
 /////////////////////
