7f92bf3c5dbbc4f16e23460d478531f4a6a58c1b
Merp <merrillamyersiii@gmail.com>
2014-11-01 13:02:18 -0400

    Added pseudocode for new ramhole scanner.

--- MerpMod/RamHoleScanner.c
+++ MerpMod/RamHoleScanner.c
@@ -16,6 +16,25 @@ if(pRamVariables->ScannedAddress > 0xBFFF)
 //pRamVariables->ScannedAddress USHORT
 //pRamVariables->ScannedValue USHORT
 //pRamVariables->ScannedtackPointer ULONG
+
+//SCANNER:
+//Initialize: set scan range 0xFFFF0000 - 0xFFFFBFFF (SH7058)
+//Set minimum byte size for ramhole.
+//Pop range from array.
+//Call range reader.
+
+//RANGE READER:
+//init vars.
+//Read @ address
+//If same as previous value, increment counter
+//Keep going.
+//
+//If minimum size is met, store the start address in array.
+//Keep going
+//When end is found (different value or range bound) store end address (and count? somewhat redundant.) in array. 
+//
+//If not at end of range, repeat range scan with new start bound!.
+//Else return
 }
 
 #endif
