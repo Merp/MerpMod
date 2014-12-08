b4a86976d71da388bf5ced526030cf4e4a85fe94
kerchakone <kerchakone@Yahoo.com>
2014-12-08 09:36:08 -0800

    Initial Commit

---
+++ MerpMod/Mileage.c
@@ -0,0 +1,35 @@
+/*	Copyright (C) 2014 Scott T Anderson & 2012-2013 Merrill A. Myers III merrillamyersiii@gmail.com
+	
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+*/
+
+#include "EcuHacks.h"
+
+#if MPG_HACKS
+
+void Mileage()
+	
+{
+	float TargetCLAFR;
+	//Lean 5th Gear Cruise
+	if (*pCurrentGear == 5 && *pEngineLoad <= LeanCruiseLoadThreshold)
+		{
+			TargetCLAFR = Pull2DHooked(&O2SensorScalingMPGTable, *pAFSensor1Current);
+		}
+	else
+		{
+			TargetCLAFR = Pull2DHooked((void*)tO2SensorScaling, *pAFSensor1Current);
+		}
+		
+	pRamVariables->TargetCLAFR = TargetCLAFR;
+}
+
+#endif
